909b7eead956a53b9becd0a7482b56503f51e627
anaglodariu <anaglodariu@gmail.com>
2023-08-27 22:18:34 +0300

    Add exceptions and arrays

--- src/inventory/ProductTester.java
+++ src/inventory/ProductTester.java
@@ -1,52 +1,54 @@
 package inventory;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ProductTester {
-    // main class
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int tempId;
-        String tempName;
-        int tempQty;
-        double tempPrice;
-        Product movie1;
-        for (int i = 0; i < 1; i++) {
-            System.out.println("Enter the product id: ");
-            tempId = in.nextInt();
-            String temp = in.nextLine();
-            System.out.println("Enter the product name: ");
-            tempName = in.nextLine();
-            //String temp = in.nextLine();
-            System.out.println("Enter the product quantity: ");
-            tempQty = in.nextInt();
-            System.out.println("Enter the product price: ");
-            tempPrice = in.nextDouble();
+        int maxSize = -1;
+        do  {
+            try {
+                System.out.println("Enter the maximum number of products you wish to store." +
+                        "\nEnter 0 (zero) if you do not wish to store any products. ");
+                maxSize = in.nextInt();
+                if (maxSize < 0) {
+                    throw new Exception();
+                }
+            } catch (InputMismatchException e1) {
+                System.out.println("Cannot enter a string. Please enter a number.");
+                // clear the buffer
+                in.nextLine();
 
-            //temp = in.nextLine();
-            movie1 = new Product(tempId, tempName, tempQty, tempPrice);
-            movie1.setActive(false);
-            System.out.println(movie1);
+            } catch (Exception e2) {
+                System.out.println("Negative value entered. Please try again.");
+                // clear the buffer
+                in.nextLine();
+            }
+        } while (maxSize < 0);
+        if (maxSize == 0) {
+            System.out.println("No products required.");
+        } else {
+            Product[] products = new Product[maxSize];
+            for (int i = 0; i < maxSize; i++) {
+                System.out.println("Enter the product id: ");
+                int tempId = in.nextInt();
+                String temp = in.nextLine();
+                System.out.println("Enter the product name: ");
+                String tempName = in.nextLine();
+                System.out.println("Enter the product quantity: ");
+                int tempQty = in.nextInt();
+                System.out.println("Enter the product price: ");
+                double tempPrice = in.nextDouble();
+                products[i] = new Product(tempId, tempName, tempQty, tempPrice);
+                products[i].setActive(false);
+
+            }
+            System.out.println("The products stored are: ");
+            for (Product p : products) {
+                System.out.println(p);
+            }
         }
         in.close();
-
-//        System.out.println("Enter the product id: ");
-//        tempId = in.nextInt();
-//        System.out.println("Enter the product name: ");
-//        tempName = in.next();
-//        String temp = in.nextLine();
-//        System.out.println("Enter the product quantity: ");
-//        tempQty = in.nextInt();
-//        System.out.println("Enter the product price: ");
-//        tempPrice = in.nextDouble();
-//        //temp = in.nextLine();
-//        Product movie1 = new Product(tempId, tempName, tempQty, tempPrice);
-//        System.out.println(movie1);
-        //        Product movie1 = new Product(1, "Ice Age 1", 1, 20);
-//        Product movie2 = new Product(2, "Ice Age 2", 1, 20);
-//        Product movie3 = new Product();
-//        System.out.println(movie1);
-//        System.out.println(movie2);
-//        System.out.println(movie3);
     }
 }
